65b581f9d30419636b90a5d7d95a82f00af6a9b2
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2023-07-11 20:21:30 +0200

    form style

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import { useContext, useEffect } from "react";
 import TokenService from "./setup/services/token.service";
 import UserService from "./setup/services/user.service";
 import MainRouter from './app/routers/MainRouter';
+import { useState } from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 function App() {
   const { setUser } = useContext(UserContext);
+  const [isAPIOnline, setIsAPIOnline] = useState(false)
 
   useEffect(() => {
+    const onlineAPI = async () => {
+      let url = process.env.REACT_APP_API_URL
+      url = url.replace('/api', '')
+      const heloWorld = await fetch(url)
+      if(heloWorld.status === 200) setIsAPIOnline(true)
+    }
+    onlineAPI()
     const userGlobal = async () => { 
       const acccessToken = TokenService.getTokenFromLocalStorage()
       if(acccessToken){
@@ -24,12 +34,34 @@ function App() {
     // eslint-disable-next-line
   }, [])
 
+  useEffect(() => {
+    console.log(isAPIOnline);
+  }, [isAPIOnline])
+
   return (
-    <BrowserRouter>
-        <MainLayout>
-            <MainRouter />
+    isAPIOnline ? (
+        <BrowserRouter>
+          <MainLayout>
+          <MainRouter />
           </MainLayout>
-    </BrowserRouter>
+        </BrowserRouter>
+      ) : 
+    (<div className='critical'>
+      <div className="bg">
+        <Player
+          autoplay
+          loop
+          src="/Dark_royal_background.json"
+          className='player'
+          style={{ height: '100vh', width: '100%' }}
+        />
+      </div>
+      <button
+        onClick={() => window.location.reload()}
+        className='btnPrimary'
+      >
+        Recharger la page
+      </button></div>)
   );
 }
 
